913aca8eb049333d2c243d4b6e682758fd220388
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-07-01 16:39:34 -0700

    and then there were two

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -3,19 +3,29 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
+        int blue = -1;
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity blueBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 18)
+                .setColorScheme(new ColorSchemeBlueDark())
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
+        RoadRunnerBotEntity redBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 18)
+                .setColorScheme(new ColorSchemeRedDark())
+                .build();
+
+        blueBot.runAction(blueBot.getDrive().actionBuilder(new Pose2d(12, 60, Math.toRadians(270)))
                 .lineToY(34.5)
                 .waitSeconds(1)
                 .strafeToSplineHeading(new Vector2d(47,34.5), Math.toRadians(0))
@@ -38,11 +48,35 @@ public class MeepMeepTesting {
                 .strafeTo(new Vector2d(59,59))
                 .build());
 
+        redBot.runAction(redBot.getDrive().actionBuilder(new Pose2d(12, 60*blue, Math.toRadians(270)))
+                .lineToY(34.5*blue)
+                .waitSeconds(1)
+                .strafeToSplineHeading(new Vector2d(47,34.5*blue), Math.toRadians(0))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-52,34.5*blue))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(47,34.5*blue))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-52,34.5*blue))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(47,34.5*blue))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-34,34.5*blue))
+                .splineTo(new Vector2d(-52,24*blue), Math.toRadians(180))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(-34,34.5*blue), Math.toRadians(0))
+                .strafeTo(new Vector2d(47,34.5*blue))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(47,59*blue))
+                .strafeTo(new Vector2d(59,59*blue))
+                .build());
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(blueBot)
+                .addEntity(redBot)
                 .start();
     }
 }
